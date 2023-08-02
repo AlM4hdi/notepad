7f53ba351af5391cdac7c87d56fc7a5ec879b457
AlM4hdi <72301856+AlM4hdi@users.noreply.github.com>
2023-08-02 19:33:32 +0600

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,8 +1,8 @@
 <?php 
-$conn = new mysqli("localhost", "id20039783_admin","E??rIC!MC#?J+2})","id20039783_pracaccounting");
+
+<!-- $conn = new mysqli("hostname", "username", "database password", "database name"); -->
 if(!$conn){
     echo "Failed to connect to the database";
 };
 ?>
 
-<!-- $conn = new mysqli("hostname", "username", "database password", "database name") -->

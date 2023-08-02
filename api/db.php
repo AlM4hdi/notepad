8ee78279b49160773013c47cc310f8f01a7777ad
AlM4hdi <72301856+AlM4hdi@users.noreply.github.com>
2023-08-02 19:33:54 +0600

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-<!-- $conn = new mysqli("hostname", "username", "database password", "database name"); -->
+ $conn = new mysqli("hostname", "username", "database password", "database name");
 if(!$conn){
     echo "Failed to connect to the database";
 };
